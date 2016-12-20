367c96b70d19caa169713bfe7ae7c7d611b71c5e
hobietime <caioangelomotta@gmail.com>
2016-12-19 21:40:19 -0800

    Add constraints to solver.  Gettting slow....

--- MPCsolver.py
+++ MPCsolver.py
@@ -1,14 +1,16 @@
 import tensorflow as tf
 
 class MPCsolver(object):
-	def __init__(self, num_steps):
+	def __init__(self, num_steps, v_limit=1, a_limit=.1, phi_limit=.2, p_slack=1):
+		"""This class can be thought of as the configuration file for the dataflow graph
+			takes a set of limits (num_steps, v_limit, a_limit, phi_limit, p_slack) 
+			on construction and requires two initial conditions(intial_v, initial_theta, targets)
+			it optimizes a series of control inputs (turn, throttle), the length of which is defined
+			by num_steps"""
 		self.initial_v = tf.placeholder(tf.float32, name="initial_speed")
-		self.initial_theta_v = tf.placeholder(tf.float32, name="initial_theta_v")
+		self.initial_phi = tf.placeholder(tf.float32, name="initial_theta")
 		self.targets = tf.placeholder(tf.float32, [2,1,None], name="targets")
-		#self.y_v = tf.placeholder(tf.float32, [num_steps,1], name="y_v")
-		#self.x_v = tf.placeholder(tf.float32, [num_steps,1], name="theta_v")
-		#self.turn = tf.placeholder(tf.float32, [num_steps, 1], name="turn")
-		#self.acc = tf.placeholder(tf.float32, [num_steps, 1], name="throttle")
+
 		self.turn = tf.get_variable(
 				"turn",
 				shape=[num_steps, 1],
@@ -18,12 +20,13 @@ class MPCsolver(object):
 				shape=[num_steps, 1],
 				initializer=tf.contrib.layers.xavier_initializer())
 
-		with tf.name_scope("input_translation":
-			self.theta_v = self.turn
-			self.v = tf.add(tf.cumsum(self.throttle), self.initial_v)
-			self.theta = tf.add(tf.cumsum(self.theta_v), self.initial_theta_v)
+		with tf.name_scope("input_translation"):
+			self.phi = self.turn
+			self.t = self.throttle
+			self.v = tf.add(tf.cumsum(self.t), self.initial_v)
 
 		with tf.name_scope("XY_tranlation"):
+			self.theta = tf.add(tf.cumsum(self.phi), self.initial_theta)
 			self.x_v = tf.multiply(self.v, tf.cos(self.theta))
 			self.y_v = tf.multiply(self.v, tf.sin(self.theta))
 
@@ -32,14 +35,20 @@ class MPCsolver(object):
 			self.xy = tf.pack([self.x,self.y])
 
 		with tf.name_scope("constraints"):
-			self.distances = tf.reduce_min(tf.reduce_max(tf.abs(tf.subtract(self.xy, self.targets)), axis=0), axis=0, name="postion_constraint")
-
+			self.target_distances = tf.reduce_min(tf.reduce_max(tf.abs(tf.subtract(self.xy, self.targets)), axis=0), axis=0, name="postion_constraint")
+			self.max_v = tf.reduce_max(tf.abs(self.v))
+			self.a = tf.add(tf.multiply(tf.abs(self.t), tf.abs(self.phi)),tf.abs(self.t))
+			self.max_a = tf.reduce_max(self.a)
+			self.max_phi = tf.reduce_max(tf.abs(self.phi))
+			self.avg_phi = tf.reduce_mean(tf.abs(self.phi))
+			self.avg_v = tf.reduce_mean(self.v)
 
 		with tf.name_scope("loss"):
-			
-			self.distance = tf.reduce_mean(self.distances)
+			self.acc_loss = tf.nn.relu(tf.subtract(self.max_a, a_limit))
+			self.velocity_loss = tf.abs(tf.subtract(self.max_v, v_limit))
+			self.phi_loss = tf.nn.relu(tf.subtract(self.max_phi, phi_limit))
+			self.postion_loss = tf.reduce_mean(tf.subtract(self.target_distances, p_slack))
 
-			self.losses = tf.reduce_min(self.x)
-			self.loss = self.distance
+			self.loss = self.postion_loss + 2*self.velocity_loss + 2*self.phi_loss + 2*self.acc_loss - self.avg_v
 			
 			
--- solvertest.py
+++ solvertest.py
@@ -7,7 +7,7 @@ turns = np.empty((20,1))
 turns.fill(.1)
 accs = np.empty((20,1))
 accs.fill(.00)
-targets = np.array([[[0, 1, 4, 5]],[[0, 1, 8, 6]]])
+targets = np.array([[[ 1, 4, 5, 12]],[[ 1, 8, 6, 12]]])
 with tf.Graph().as_default():
 	sess = tf.Session()
 	with sess.as_default():
@@ -22,25 +22,27 @@ with tf.Graph().as_default():
 		summary_writer = tf.train.SummaryWriter(".", graph=tf.get_default_graph())
 		feed_dict = {
 			solver.initial_v: 1,
-			solver.initial_theta_v: 0,
+			solver.initial_phi: 0,
 			solver.targets: targets,
 			#solver.acc: accs
 		}
 		tf.scalar_summary("loss", solver.loss)
 		merged_summary_op = tf.merge_all_summaries()
 
-		_, summary, xpos, ypos, distances = sess.run( [global_step, merged_summary_op, solver.x, solver.y, solver.distances], feed_dict)
+		_, summary, xpos, ypos, distances = sess.run( [global_step, merged_summary_op, solver.x, solver.y, solver.v], feed_dict)
 		summary_writer.add_summary(summary, 1)
 		plt.plot(targets[1,0,:], targets[0,0,:], 'ro')
-		plt.plot(ypos, xpos, label = 'Parametric Curve')
+		plt.plot(ypos, xpos, 'bo')
+		plt.plot(ypos, xpos)
 		plt.axis('equal')
 		plt.show()
-		print(distances)
-		for i in range(200):
-			_, itnum, xpos, ypos, distances = sess.run( [train_op, global_step, solver.x, solver.y, solver.distances], feed_dict)
-			print(distances)
-
-		plt.plot(ypos, xpos, label = 'Parametric Curve')
+		#print(distances)
+		for i in range(2000):
+			_, itnum, xpos, ypos, distances = sess.run( [train_op, global_step, solver.x, solver.y, solver.phi], feed_dict)
+			#print(distances)
+		print("done")
+		plt.plot(ypos, xpos, 'bo')
+		plt.plot(ypos, xpos)
 		plt.plot(targets[1,0,:], targets[0,0,:], 'ro')
 		plt.axis('equal')
 		plt.show()
