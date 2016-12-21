4c1053399095b510f6a042ee02251e4f1e752fb1
hobietime <caioangelomotta@gmail.com>
2016-12-21 15:58:13 -0800

    fix centripital acceleration bug

--- MPCsolver.py
+++ MPCsolver.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+import numpy as np
 
 class MPCsolver(object):
-	def __init__(self, num_steps, v_limit=1, a_limit=.1, phi_limit=.2, p_slack=1):
+	def __init__(self, num_steps, v_limit=2, a_limit=.01, phi_limit=.2, p_slack=1):
 		"""This class can be thought of as the configuration file for the dataflow graph
 			takes a set of limits (num_steps, v_limit, a_limit, phi_limit, p_slack) 
 			on construction and requires two initial conditions(intial_v, initial_theta, targets)
@@ -11,14 +12,12 @@ class MPCsolver(object):
 		self.initial_theta = tf.placeholder(tf.float32, name="initial_theta")
 		self.targets = tf.placeholder(tf.float32, [2,1,None], name="targets")
 
-		self.turn = tf.get_variable(
-				"turn",
-				shape=[num_steps, 1],
-				initializer=tf.contrib.layers.xavier_initializer())
-		self.throttle = tf.get_variable(
-				"throttle",
-				shape=[num_steps, 1],
-				initializer=tf.contrib.layers.xavier_initializer())
+		self.turn = tf.Variable(
+				name="turn",
+				initial_value=np.zeros(num_steps,  dtype=np.float32).reshape(num_steps,1))
+		self.throttle = tf.Variable(
+				name="throttle",
+				initial_value=np.zeros(num_steps,  dtype=np.float32).reshape(num_steps,1))
 
 		with tf.name_scope("input_translation"):
 			self.phi = self.turn
@@ -37,7 +36,7 @@ class MPCsolver(object):
 		with tf.name_scope("constraints"):
 			self.target_distances = tf.reduce_min(tf.reduce_max(tf.abs(tf.subtract(self.xy, self.targets)), axis=0), axis=0, name="postion_constraint")
 			self.max_v = tf.reduce_max(tf.abs(self.v))
-			self.a = tf.add(tf.multiply(tf.abs(self.t), tf.abs(self.phi)),tf.abs(self.t))
+			self.a = tf.add(tf.multiply(tf.square(tf.abs(self.t)), tf.abs(self.phi)),tf.abs(self.t))
 			self.max_a = tf.reduce_max(self.a)
 			self.max_phi = tf.reduce_max(tf.abs(self.phi))
 			self.avg_phi = tf.reduce_mean(tf.abs(self.phi))
@@ -47,8 +46,8 @@ class MPCsolver(object):
 			self.acc_loss = tf.nn.relu(tf.subtract(self.max_a, a_limit))
 			self.velocity_loss = tf.abs(tf.subtract(self.max_v, v_limit))
 			self.phi_loss = tf.nn.relu(tf.subtract(self.max_phi, phi_limit))
-			self.postion_loss = tf.reduce_mean(tf.subtract(self.target_distances, p_slack))
+			self.postion_loss = tf.reduce_mean(tf.nn.relu(tf.subtract(self.target_distances, p_slack)))
 
-			self.loss = self.postion_loss + 2*self.velocity_loss + 2*self.phi_loss + 2*self.acc_loss - self.avg_v
+			self.loss = 2*self.postion_loss + 2*self.velocity_loss + 2*self.phi_loss + 2*self.acc_loss - self.avg_v
 			
 			
--- solvertest.py
+++ solvertest.py
@@ -7,14 +7,14 @@ turns = np.empty((20,1))
 turns.fill(.1)
 accs = np.empty((20,1))
 accs.fill(.00)
-targets = np.array([[[ 1, 4, 5, 12]],[[ 1, 8, 6, 12]]])
+targets = np.array([[[ 1, 4, 5, 0, -5, -10]],[[ 1, 9, 6, 10, 9, 10]]])
 with tf.Graph().as_default():
 	sess = tf.Session()
 	with sess.as_default():
 		solver = MPCsolver(20)
 		global_step = tf.Variable(0, name="global_step", trainable=False)
 
-		optimizer = tf.train.AdamOptimizer(1e-2)
+		optimizer = tf.train.AdamOptimizer(1e-1)
 		grads_and_vars = optimizer.compute_gradients(solver.loss)
 		train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
@@ -31,18 +31,21 @@ with tf.Graph().as_default():
 
 		_, summary, xpos, ypos, distances = sess.run( [global_step, merged_summary_op, solver.x, solver.y, solver.v], feed_dict)
 		summary_writer.add_summary(summary, 1)
-		plt.plot(targets[1,0,:], targets[0,0,:], 'ro')
+		#plt.plot(targets[1,0,:], targets[0,0,:], 'ro',  xerr=1, yerr=1)
 		plt.plot(ypos, xpos, 'bo')
 		plt.plot(ypos, xpos)
 		plt.axis('equal')
 		plt.show()
 		#print(distances)
-		for i in range(1000):
-			_, itnum, xpos, ypos, distances = sess.run( [train_op, global_step, solver.x, solver.y, solver.phi], feed_dict)
-			#print(distances)
+		for i in range(100):
+			_, itnum, xpos, ypos, acc_loss, velocity_loss, phi_loss, postion_loss, avg_phi, avg_v, loss = sess.run( \
+				[train_op, global_step, solver.x, solver.y, solver.acc_loss, solver.velocity_loss, \
+				solver.phi_loss, solver.postion_loss, solver.avg_phi, solver.avg_v, solver.loss], feed_dict)
+		print("acc_loss, velocity_loss, phi_loss, postion_loss, avg_phi, avg_v, loss")
+		print(acc_loss, velocity_loss, phi_loss, postion_loss, avg_phi, avg_v, loss)
 		print("done")
 		plt.plot(ypos, xpos, 'bo')
 		plt.plot(ypos, xpos)
-		plt.plot(targets[1,0,:], targets[0,0,:], 'ro')
+		plt.errorbar(targets[1,0,:], targets[0,0,:], xerr=1.0, yerr=1.0, fmt='o')
 		plt.axis('equal')
 		plt.show()
